f17bb8415423c1d532d2c60ac6fdf62c9378606d
Josh Headapohl <joshhead@gmail.com>
2020-08-01 21:24:39 -0400

    Add tinymodule to experiment with nmigen.sim.pysim

---
+++ joybusutils/tinymodule.py
@@ -0,0 +1,41 @@
+# This is an example module to help me get started with nmigen
+# It should be a module that sets a signal high after 5 clock cycles.
+
+from nmigen import Elaboratable, Shape, Signal, Module, ClockDomain
+from nmigen.sim.pysim import Simulator, Tick, Delay
+from tabulate import tabulate
+
+class TinyModule(Elaboratable):
+    def __init__(self):
+        self.counter = Signal(5)
+        self.output = Signal()
+
+    def elaborate(self, platform):
+        m = Module()
+        m.d.sync += self.counter.eq(self.counter + 1)
+        # Once high, output should stay high.
+        with m.If(self.output == 1):
+            m.d.sync += self.output.eq(1)
+        # Otherwise, wait for 5 clock ticks
+        with m.Elif(self.counter == 5):
+            m.d.sync += self.output.eq(1)
+        return m
+
+
+if __name__ == "__main__":
+    dut = TinyModule()
+    sim = Simulator(dut)
+
+    sim_results = []
+
+    def process():
+        # Enough ticks for the counter to overflow
+        for i in range(35):
+            sim_results.append([i, (yield dut.counter), (yield dut.output)])
+            yield Tick()
+    sim.add_sync_process(process)
+    # Not 100% sure how this works. Intent is a 16mhz clock.
+    sim.add_clock(1.6e7)
+    sim.run()
+
+    print(tabulate(sim_results, headers=["Clock", "Counter", "Output"]))
