005ae9be0677b5466375929cb4b258f3ef784c9a
Josh Headapohl <joshhead@gmail.com>
2020-08-02 21:03:57 -0400

    Add unit test for tinymodule

---
+++ joybusutils/test_tinymodule.py
@@ -0,0 +1,35 @@
+from nmigen import Module
+from nmigen.sim.pysim import Simulator, Tick
+from tinymodule import TinyModule
+from nmigen.test.utils import FHDLTestCase
+import unittest
+
+CLOCK_PERIOD = 1/1_200_000
+
+
+class TinyModuleTest(FHDLTestCase):
+    def setUp(self):
+        self.tinymodule = TinyModule()
+        self.sim = Simulator(self.tinymodule)
+        self.sim.add_clock(CLOCK_PERIOD)
+
+    def test_output_starts_at_0(self):
+        def process():
+            self.assertEqual((yield self.tinymodule.output), 0)
+        self.sim.add_sync_process(process)
+        self.sim.run()
+
+    def test_output_toggles_after5_ticks(self):
+        def process():
+            for _ in range(5):
+                yield Tick()
+                self.assertEqual((yield self.tinymodule.output), 0)
+            for _ in range(32):
+                yield Tick()
+                self.assertEqual((yield self.tinymodule.output), 1)
+        self.sim.add_sync_process(process)
+        self.sim.run()
+
+
+if __name__ == "__main__":
+    unittest.main()
