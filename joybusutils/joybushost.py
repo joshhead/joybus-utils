e64570a3faf0eb39d9fb87f357bdcdd5afc9dfd0
Josh Headapohl <joshhead@gmail.com>
2020-08-04 22:44:56 -0400

    Listen for write signal in state machine

--- joybusutils/joybushost.py
+++ joybusutils/joybushost.py
@@ -8,27 +8,23 @@ class JoyBusHost(Elaboratable):
     def __init__(self):
         self.counter = Signal(10)
         self.output = Signal(reset=1)
+        self.write = Signal()
 
     def ports(self):
-        return [self.output]
+        return [self.output, self.write]
 
     def elaborate(self, platform):
         m = Module()
         with m.FSM():
-            with m.State("START"):
-                with m.If(self.counter == 10):
-                    m.d.sync += self.counter.eq(0)
+            with m.State("IDLE"):
+                m.d.comb += self.output.eq(1)
+                m.d.sync += self.counter.eq(0)
+                with m.If(self.write == 1):
                     m.next = "0"
-                with m.Elif(self.counter > 5):
-                    m.d.comb += self.output.eq(1)
-                    m.d.sync += self.counter.eq(self.counter + 1)
-                with m.Else():
-                    m.d.comb += self.output.eq(0)
-                    m.d.sync += self.counter.eq(self.counter + 1)
             with m.State("0"):
                 with m.If(self.counter == 48):
                     m.d.sync += self.counter.eq(0)
-                    m.next = "START"
+                    m.next = "IDLE"
                 with m.Elif(self.counter > 36):
                     m.d.comb += self.output.eq(1)
                     m.d.sync += self.counter.eq(self.counter + 1)
@@ -45,7 +41,11 @@ if __name__ == "__main__":
     # There is a sim.run_until() that should let me run without a process,
     # but I can't figure out how to get it working.
     def process():
-        for _ in range(3000):
+        for i in range(3000):
+            if (i == 10):
+                yield joybushost.write.eq(1)
+            elif (i == 11):
+                yield joybushost.write.eq(0)
             yield Tick()
     sim.add_sync_process(process)
     sim.add_clock(CLOCK_PERIOD)
