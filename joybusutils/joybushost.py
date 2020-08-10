7f2b42c5e46a549230c1c6073b6ed6d3f9ce837b
Josh Headapohl <joshhead@gmail.com>
2020-08-09 20:26:19 -0400

    Transmit data_in one bit at a time

--- joybusutils/joybushost.py
+++ joybusutils/joybushost.py
@@ -2,13 +2,13 @@ from nmigen import Elaboratable, Signal, Module
 from nmigen.sim.pysim import Simulator, Tick
 
 CLOCK_PERIOD = 1/12_000_000
-DATA_WIDTH = 2
+DATA_WIDTH = 4
 
 class JoyBusHost(Elaboratable):
     def __init__(self):
         self.counter = Signal(10) # For timing
         self.data = Signal(DATA_WIDTH)
-        self.tx_index = Signal(DATA_WIDTH)
+        self.tx_bits_remaining = Signal(DATA_WIDTH)
         self.tx = Signal(reset=1) # Serialized output
         self.data_in = Signal(DATA_WIDTH) # Data in, to serialize
         self.write_enable = Signal() # Strobe high to latch data and send
@@ -25,15 +25,25 @@ class JoyBusHost(Elaboratable):
                 m.d.comb += self.tx.eq(1) # Idle high
                 m.d.sync += self.counter.eq(0)
                 m.d.sync += self.data.eq(self.data_in)
-                m.d.sync += self.tx_index.eq(DATA_WIDTH - 1)
+                m.d.sync += self.tx_bits_remaining.eq(DATA_WIDTH - 1)
                 with m.If(self.write_enable == 1):
-                    m.next = "TX_0"
+                    with m.If(self.data_in[0] == 0):
+                        m.next = "TX_0"
+                    with m.Else():
+                        m.next = "TX_1"
             with m.State("TX_0"):
                 m.d.comb += self.busy.eq(1)
                 with m.If(self.counter == 48):
                     m.d.sync += self.counter.eq(0)
-                    m.d.sync += self.tx_index.eq(self.tx_index - 1)
-                    m.next = "TX_1"
+                    m.d.sync += self.tx_bits_remaining.eq(self.tx_bits_remaining - 1)
+                    next_data = self.data.shift_right(1)
+                    m.d.sync += self.data.eq(next_data)
+                    with m.If(self.tx_bits_remaining == 0):
+                        m.next = "IDLE"
+                    with m.Elif(next_data[0] == 0):
+                        m.next = "TX_0"
+                    with m.Else():
+                        m.next = "TX_1"
                 with m.Elif(self.counter > 36):
                     m.d.comb += self.tx.eq(1)
                     m.d.sync += self.counter.eq(self.counter + 1)
@@ -44,8 +54,15 @@ class JoyBusHost(Elaboratable):
                 m.d.comb += self.busy.eq(1)
                 with m.If(self.counter == 48):
                     m.d.sync += self.counter.eq(0)
-                    m.d.sync += self.tx_index.eq(self.tx_index - 1)
-                    m.next = "IDLE"
+                    m.d.sync += self.tx_bits_remaining.eq(self.tx_bits_remaining - 1)
+                    next_data = self.data.shift_right(1)
+                    m.d.sync += self.data.eq(next_data)
+                    with m.If(self.tx_bits_remaining == 0):
+                        m.next = "IDLE"
+                    with m.Elif(next_data[0] == 0):
+                        m.next = "TX_0"
+                    with m.Else():
+                        m.next = "TX_1"
                 with m.Elif(self.counter > 12):
                     m.d.comb += self.tx.eq(1)
                     m.d.sync += self.counter.eq(self.counter + 1)
@@ -62,6 +79,7 @@ if __name__ == "__main__":
     # There is a sim.run_until() that should let me run without a process,
     # but I can't figure out how to get it working.
     def process():
+        yield joybushost.data_in.eq(0b1101)
         for i in range(3000):
             if (i == 10):
                 yield joybushost.write_enable.eq(1)
